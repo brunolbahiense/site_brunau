99a78207b679c11c7fc46b87e344d279641ddd28
brunolbahiense <brunoluisbahiense@gmail.com>
2023-07-25 16:41:57 -0300

    feat/organization update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
-import Main from 'components/Main'
+import Header from 'components/Header'
 
 export default function Home() {
-  return <Main />
+  return (
+    <>
+      <Header Logo={'Brunau Ink'} />
+    </>
+  )
 }
